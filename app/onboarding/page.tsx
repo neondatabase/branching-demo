ba679214c17293ab1021e4e67f9fd87524a6ad95
Rishi Raj Jain <jain71000@gmail.com>
2024-07-12 01:26:20 +0530

    add label

--- app/onboarding/page.tsx
+++ app/onboarding/page.tsx
@@ -2,6 +2,7 @@
 
 import { Button } from '@/components/ui/button'
 import { cn } from '@/lib/utils'
+import { CircleMinus, CirclePlus, TimerReset } from 'lucide-react'
 import { Fragment, ReactElement, useState } from 'react'
 
 interface Stage {
@@ -11,6 +12,7 @@ interface Stage {
   prev?: boolean
   rightView?: ReactElement
   leftView?: ReactElement
+  label?: string
 }
 
 export default function Onboarding() {
@@ -19,74 +21,130 @@ export default function Onboarding() {
   const [prevOn, setPrevOn] = useState(true)
   const stages: Stage[] = [
     {
-      icon: 'https://www.svgrepo.com/show/522226/play.svg',
+      label: 'Welcome',
+      icon: 'https://www.svgrepo.com/show/521608/document.svg',
       branched: false,
       leftView: (
         <div className="contents">
-          <h1 className="tracking-extra-tight text-balance text-[32px] font-semibold leading-[0.9] text-white lg:text-[44px] xl:text-[56px]">Copy database in milliseconds</h1>
-          <h2 className="tracking-extra-tight mt-[18px] text-balance text-xl font-light text-[#AFB1B6] md:mt-2 lg:mt-3 lg:text-base xl:mt-4 xl:text-lg">
-            Instantly provison a copy of your Postgres database on&nbsp;
-            <a className="text-white underline underline-offset-4 hover:no-underline" href="https://console.neon.tech/signup">
+          <h1 className="text-3xl font-semibold text-white">Copy database in milliseconds</h1>
+          <span className="mt-3 font-light text-gray-400">
+            In this demo, you will create a copy of your database, make changes in it, and restore it to the original copy in few clicks. Behind the scenes, you are leveraging the
+            instant&nbsp;
+            <a className="text-white/75 hover:underline hover:underline-offset-4" href="https://console.neon.tech/signup">
               Neon
-            </a>
-            .
-          </h2>
+            </a>{' '}
+            branching.
+          </span>
+          <Button
+            onClick={() => {
+              setStage((stage) => stage + 1)
+            }}
+            className="mt-8 max-w-max bg-[#00e599]"
+          >
+            {"Let's"} begin &rarr;
+          </Button>
         </div>
       ),
-      next: true,
-      prev: false,
     },
     {
+      label: 'Origin database',
+      icon: 'https://www.svgrepo.com/show/471315/database-02.svg',
+      branched: false,
+      leftView: (
+        <div className="contents">
+          <span className="text-xl font-medium">Branching Feature</span>
+          <span className="mt-3 text-gray-400">Instantly branch your production database to create a staging environment for testing out schema changes.</span>
+          <Button
+            onClick={() => {
+              setStage((stage) => stage + 1)
+            }}
+            className="mt-8 max-w-max"
+          >
+            <svg height="16" viewBox="0 0 16 16" version="1.1" width="16" className="mr-2 fill-black">
+              <path d="M5 5.372v.878c0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75v-.878a2.25 2.25 0 1 1 1.5 0v.878a2.25 2.25 0 0 1-2.25 2.25h-1.5v2.128a2.251 2.251 0 1 1-1.5 0V8.5h-1.5A2.25 2.25 0 0 1 3.5 6.25v-.878a2.25 2.25 0 1 1 1.5 0ZM5 3.25a.75.75 0 1 0-1.5 0 .75.75 0 0 0 1.5 0Zm6.75.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm-3 8.75a.75.75 0 1 0-1.5 0 .75.75 0 0 0 1.5 0Z"></path>
+            </svg>
+            Create a copy &rarr;
+          </Button>
+        </div>
+      ),
+      rightView: <span>table data</span>,
+    },
+    {
+      label: 'Copy database',
       icon: 'https://www.svgrepo.com/show/445570/branch.svg',
       branched: true,
       leftView: (
         <div className="contents">
-          <span className="text-xl font-medium">Test's Branching Feature</span>
+          <span className="text-xl font-medium">Branching Feature</span>
           <span className="mt-3 text-gray-400">Instantly branch your production database to create a staging environment for testing out schema changes.</span>
+          <Button
+            variant="destructive"
+            onClick={() => {
+              setStage((stage) => stage + 1)
+            }}
+            className="mt-8 max-w-max"
+          >
+            <CircleMinus size="18" />
+            <span className="ml-3">Remove a Row</span>
+          </Button>
         </div>
       ),
       rightView: <span>table data</span>,
-      next: true,
-      prev: true,
     },
     {
+      label: 'Edited table',
       icon: 'https://www.svgrepo.com/show/449277/subtract.svg',
       branched: true,
       leftView: (
         <div className="contents">
-          <span className="text-xl font-medium">Test's Branching Feature</span>
+          <span className="text-xl font-medium">Branching Feature</span>
           <span className="mt-3 text-gray-400">Instantly branch your production database to create a staging environment for testing out schema changes.</span>
+          <Button
+            onClick={() => {
+              setStage((stage) => stage + 1)
+            }}
+            className="mt-8 max-w-max bg-green-400"
+          >
+            <CirclePlus size="18" />
+            <span className="ml-3">Add a Row</span>
+          </Button>
         </div>
       ),
       rightView: <span>table data</span>,
-      next: true,
-      prev: true,
     },
     {
+      label: 'Edited table',
       icon: 'https://www.svgrepo.com/show/532994/plus.svg',
       branched: true,
       leftView: (
         <div className="contents">
-          <span className="text-xl font-medium">Test's Branching Feature</span>
+          <span className="text-xl font-medium">Branching Feature</span>
           <span className="mt-3 text-gray-400">Instantly branch your production database to create a staging environment for testing out schema changes.</span>
+          <Button
+            onClick={() => {
+              setStage((stage) => stage + 1)
+            }}
+            className="mt-8 max-w-max bg-blue-400"
+          >
+            <TimerReset size="18" />
+            <span className="ml-3">Restore the database</span>
+          </Button>
         </div>
       ),
       rightView: <span>table data</span>,
-      next: true,
-      prev: true,
     },
     {
+      label: 'Restored table',
       icon: 'https://www.svgrepo.com/show/521807/restore.svg',
       branched: false,
       leftView: (
         <div className="contents">
-          <span className="text-xl font-medium">Test's Branching Feature</span>
+          <span className="text-xl font-medium">Branching Feature</span>
           <span className="mt-3 text-gray-400">Instantly branch your production database to create a staging environment for testing out schema changes.</span>
         </div>
       ),
       rightView: <span>table data</span>,
       next: false,
-      prev: true,
     },
   ]
   const [stageLength, setStageLength] = useState(stages.length)
@@ -121,13 +179,16 @@ export default function Onboarding() {
             </div>
             <div
               className={cn(
-                'mx-8 flex size-[80px] flex-col items-center justify-center rounded-full border',
+                'relative mx-8 flex size-[80px] flex-col items-center justify-center rounded-full border',
                 _ !== stage ? 'bg-white/10 opacity-50' : 'bg-white',
                 stages[_].branched && 'mt-12',
               )}
             >
               {/* eslint-disable-next-line @next/next/no-img-element */}
               <img className="w-[30px] translate-x-0.5" src={stages[_].icon} alt="ChartJS" />
+              {stages[stage].label && (
+                <span className={cn('absolute -bottom-8 z-20 min-w-max max-w-max', _ === stage ? 'text-white' : 'text-white/10 opacity-10')}>{stages[stage].label}</span>
+              )}
             </div>
           </Fragment>
         ))}
@@ -139,8 +200,8 @@ export default function Onboarding() {
       <div className="mt-12 flex flex-row items-center gap-x-3">
         <Button
           variant="outline"
-          disabled={!prevOn || (stages[stage].hasOwnProperty('prev') && stages[stage].prev === false)}
-          className={cn((!prevOn || (stages[stage].hasOwnProperty('prev') && stages[stage].prev === false)) && 'hidden', 'bg-transparent')}
+          disabled={!prevOn || Boolean(stages[stage].prev) === false}
+          className={cn((!prevOn || Boolean(stages[stage].prev) === false) && 'hidden', 'bg-transparent')}
           onClick={() => {
             setStage((stage) => {
               const tmp = stage - 1
@@ -153,8 +214,8 @@ export default function Onboarding() {
         </Button>
         <Button
           variant="outline"
-          disabled={!nextOn || (stages[stage].hasOwnProperty('next') && stages[stage].next === false)}
-          className={cn((!nextOn || (stages[stage].hasOwnProperty('next') && stages[stage].next === false)) && 'hidden', 'bg-transparent')}
+          disabled={!nextOn || Boolean(stages[stage].next) === false}
+          className={cn((!nextOn || Boolean(stages[stage].next) === false) && 'hidden', 'bg-transparent')}
           onClick={() => {
             setStage((stage) => {
               const tmp = (stage + 1) % stageLength
