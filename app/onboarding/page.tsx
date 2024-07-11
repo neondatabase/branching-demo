a48d69499f5cad1a3d9bb8d37071a2afb2bcee5f
Rishi Raj Jain <jain71000@gmail.com>
2024-07-11 12:19:56 +0530

    prev and next

--- app/onboarding/page.tsx
+++ app/onboarding/page.tsx
@@ -12,6 +12,37 @@ export default function Onboarding() {
           <div key={_} className={'rounded-full ' + (stage !== _ ? 'size-[6px] bg-white/50' : 'size-[8px] bg-white')} />
         ))}
       </div>
+      <div className="mt-12 flex flex-row items-center gap-x-3">
+        {new Array(stageLength).fill(0).map((i, _) => (
+          <div key={_} className={'flex size-[70px] flex-col items-center justify-center rounded-full border ' + (_ !== stage ? 'bg-white/10 opacity-10' : 'bg-white')}>
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img className="w-[30px]" src="https://cdn.svgporn.com/logos/chartjs.svg" alt="ChartJS" />
+          </div>
+        ))}
+      </div>
+      <div className="flex flex-row items-center gap-x-3">
+        <button
+          onClick={() => {
+            setStage((stage) => {
+              const tmp = stage - 1
+              if (tmp < 0) return stageLength - 1
+              return tmp % stageLength
+            })
+          }}
+        >
+          Prev
+        </button>
+        <button
+          onClick={() => {
+            setStage((stage) => {
+              const tmp = (stage + 1) % stageLength
+              return tmp
+            })
+          }}
+        >
+          Next
+        </button>
+      </div>
     </div>
   )
 }
