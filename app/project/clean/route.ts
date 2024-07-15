57aaa313a19e8eb765bc8f049062929f89f5357c
Rishi Raj Jain <jain71000@gmail.com>
2024-07-15 21:30:44 +0530

    clean branch

--- app/project/clean/route.ts
+++ app/project/clean/route.ts
@@ -2,23 +2,29 @@ export const dynamic = 'force-dynamic'
 
 export const fetchCache = 'force-no-store'
 
-import { NextResponse } from 'next/server'
 import { verifySignatureAppRouter } from '@upstash/qstash/dist/nextjs'
+import { NextResponse } from 'next/server'
 
 async function handler(request: Request) {
   const data = await request.json()
   if (data?.new_branch_id) {
-    const headers = new Headers()
-    headers.append('Accept', 'application/json')
-    headers.append('Content-Type', 'application/json')
-    headers.append('Authorization', `Bearer ${process.env.NEON_API_KEY}`)
-    await fetch(`https://console.neon.tech/api/v2/projects/${process.env.NEON_PROJECT_ID}/branches/${data.new_branch_id}`, {
-      method: 'DELETE',
-      headers,
-    })
-    return NextResponse.json({ message: `Deleted branch ${data.new_branch_id} succesfully.` }, { status: 200 })
+    try {
+      const headers = new Headers()
+      headers.append('Accept', 'application/json')
+      headers.append('Content-Type', 'application/json')
+      headers.append('Authorization', `Bearer ${process.env.NEON_API_KEY}`)
+      await fetch(`https://console.neon.tech/api/v2/projects/${process.env.NEON_PROJECT_ID}/branches/${data.new_branch_id}`, {
+        method: 'DELETE',
+        headers,
+      })
+      return NextResponse.json({ message: `Deleted branch ${data.new_branch_id} succesfully.` }, { status: 200 })
+    } catch (e) {
+      // @ts-ignore
+      console.log(e.message || e.toString())
+      return NextResponse.json({ message: `Failed to delete branch ${data.new_branch_id}.` }, { status: 500 })
+    }
   }
-  return NextResponse.json({ message: `Failed to delete branch ${data.new_branch_id}.` }, { status: 500 })
+  return new NextResponse(null, { status: 400 })
 }
 
 export const POST = verifySignatureAppRouter(handler)
